2135e586bbd1ea4fce1612ef0e4ff58093aa1b93
levkopo <levkopoag@gmail.com>
2025-11-10 21:22:08 +0500

    Update sync script

--- sync.sh
+++ sync.sh
@@ -1,15 +1,42 @@
-mkdir -p /tmp/dotfiles
-git clone https://github.com/levkopo/dotfiles /tmp/dotfiles
+#!/bin/bash
+set -e
 
-cd /tmp/dotfiles
-rm -rf ~/.config/hypr
-rm -rf ~/.config/ags
-rm -rf ~/.config/fastfetch
-cp ./configs/* ~/.config
+DOTFILES_REPO="https://github.com/levkopo/dotfiles"
+DOTFILES_DIR="/usr/share/levkopo-dotfiles"
+BACKUP_DIR="$HOME/.config/config-backups/$(date +%F_%T)"
 
+echo "Клонирование репозитория в $DOTFILES_DIR..."
+[ -d "$DOTFILES_DIR" ] && rm -rf "$DOTFILES_DIR"
+git clone "$DOTFILES_REPO" "$DOTFILES_DIR"
+cd "$DOTFILES_DIR"
+
+echo "Создание резервной копии старых конфигов в $BACKUP_DIR..."
+mkdir -p "$BACKUP_DIR"
+
+CONFIGS=(ags fastfetch hypr)
+
+for config in "${CONFIGS[@]}"; do
+    if [ -d "$HOME/.config/$config" ]; then
+        echo "Бэкап конфига: $config"
+        mv "$HOME/.config/$config" "$BACKUP_DIR/"
+    fi
+    echo "Создание символической ссылки для $config..."
+    ln -s "$DOTFILES_DIR/configs/$config" "$HOME/.config/$config"
+done
+
+echo "Пользовательские конфиги успешно установлены."
+
+echo "Настройка GRUB..."
 sudo cp ./grub/default/grub /etc/default/grub
-sudo cp -r ./grub/themes /usr/share/grub
+sudo cp -r ./grub/themes/* /usr/share/grub/themes/
+echo "Пересборка конфигурации GRUB. Это может занять некоторое время..."
 sudo grub-mkconfig -o /boot/grub/grub.cfg
+echo "GRUB успешно настроен."
 
-sudo cp -r ./plymouth/themes /usr/share/plymouth
-sudo cp ./plymouth/plymouthd.conf /etc/plymouth
+echo "Настройка Plymouth..."
+sudo cp -r ./plymouth/themes/* /usr/share/plymouth/themes/
+sudo cp ./plymouth/plymouthd.conf /etc/plymouth/plymouthd.conf
+sudo mkinitcpio -P
+
+echo "Plymouth успешно настроен."
+echo "Установка завершена! Перезагрузите систему, чтобы увидеть все изменения."
