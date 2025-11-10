a94b17a4b45f47968bea349f3f8722881d7eb099
levkopo <levkopoag@gmail.com>
2025-11-10 21:47:58 +0500

    Update sync and install scripts

--- sync.sh
+++ sync.sh
@@ -1,54 +1,40 @@
 #!/bin/bash
-set -e
 
-DOTFILES_REPO="https://github.com/levkopo/dotfiles"
 DOTFILES_DIR="$HOME/.config/levkopo-dotfiles"
-BACKUP_DIR="$HOME/.config/config-backups/$(date +%F_%T)"
 
-echo "Клонирование репозитория в $DOTFILES_DIR..."
-[ -d "$DOTFILES_DIR" ] && rm -rf "$DOTFILES_DIR"
-mkdir -p "$DOTFILES_DIR"
-chmod 755 "$DOTFILES_DIR"
-git clone "$DOTFILES_REPO" "$DOTFILES_DIR"
-cd "$DOTFILES_DIR"
+cd "$DOTFILES_DIR" || exit 1
 
-echo "Создание резервной копии старых конфигов в $BACKUP_DIR..."
-mkdir -p "$BACKUP_DIR"
+git fetch
+BEFORE=$(git rev-parse HEAD)
+git pull
+AFTER=$(git rev-parse HEAD)
 
-CONFIGS=(ags fastfetch hypr)
+if [ "$BEFORE" == "$AFTER" ]; then
+    echo "Дотфайлы в актуальном состоянии. Выход."
+    exit 0
+fi
 
-for config in "${CONFIGS[@]}"; do
-    if [ -d "$HOME/.config/$config" ]; then
-        echo "Бэкап конфига: $config"
-        mv "$HOME/.config/$config" "$BACKUP_DIR/"
-    fi
-    echo "Создание символической ссылки для $config..."
-    ln -s "$DOTFILES_DIR/configs/$config" "$HOME/.config/$config"
-done
+echo "Обнаружены обновления в дотфайлах. Применяем изменения..."
 
-hyprctl reload
+if ! git diff --quiet "$BEFORE" "$AFTER" -- ./grub/default/grub; then
+    echo "Обнаружены изменения в конфигурации GRUB. Обновление..."
+    sudo cp "$DOTFILES_DIR/grub/default/grub" /etc/default/grub
+    sudo cp -r "$DOTFILES_DIR/grub/themes"/* /usr/share/grub/themes/
+    sudo grub-mkconfig -o /boot/grub/grub.cfg
+fi
 
-echo "Пользовательские конфиги успешно установлены."
+if ! git diff --quiet "$BEFORE" "$AFTER" -- ./plymouth; then
+    echo "Обнаружены изменения в конфигурации Plymouth. Обновление..."
+    sudo cp -r "$DOTFILES_DIR/plymouth/themes"/* /usr/share/plymouth/themes/
+    sudo cp "$DOTFILES_DIR/plymouth/plymouthd.conf" /etc/plymouth/plymouthd.conf
+    sudo mkinitcpio -P
+fi
 
-echo "Настройка GRUB..."
-sudo cp ./grub/default/grub /etc/default/grub
-sudo cp -r ./grub/themes/* /usr/share/grub/themes/
-echo "Пересборка конфигурации GRUB. Это может занять некоторое время..."
-sudo grub-mkconfig -o /boot/grub/grub.cfg
-echo "GRUB успешно настроен."
+if ! git diff --quiet "$BEFORE" "$AFTER" -- ./sddm; then
+    echo "Обнаружены изменения в конфигурации SDDM. Обновление..."
+    sudo cp -r "$DOTFILES_DIR/sddm/themes"/* /usr/share/sddm/themes/
+    sudo cp "$DOTFILES_DIR/sddm/sddm.conf" /etc/sddm.conf
+    sudo mkinitcpio -P
+fi
 
-echo "Настройка Plymouth..."
-sudo cp -r ./plymouth/themes/* /usr/share/plymouth/themes/
-sudo cp ./plymouth/plymouthd.conf /etc/plymouth/plymouthd.conf
-sudo mkinitcpio -P
-
-echo "Plymouth успешно настроен."
-
-echo "Настройка SDDM..."
-sudo cp -r ./sddm/themes/* /usr/share/sddm/themes/
-sudo cp ./sddm/sddm.conf /etc/sddm.conf
-sudo mkinitcpio -P
-
-echo "SDDM успешно настроен."
-
-echo "Установка завершена! Перезагрузите систему, чтобы увидеть все изменения."
+echo "Синхронизация завершена."
